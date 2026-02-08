76047ffc7b72f4223fa4af6c094b2f67701f5ece
raphlla <13264747680@163.com>
2026-02-09 00:36:33 +0800

    fix(2ch): restore index subtitles for all threads and bump cache tag

--- 2ch/threads/index.js
+++ 2ch/threads/index.js
@@ -8,19 +8,19 @@ app.setThreadList({
         },
         {
             "id": "ayap_yachiyo_collab",
-            "title": "【悲報】いろP、ヤチヨとコラボ確定www いろかぐ厨息してる？ww",
+            "title": "【悲報】いろP、ヤチヨとコラボ確定www いろかぐ厨息してる？ww<div class=\"title-subtitle\">【悲报】彩P确定与ヤチヨ联动www 彩辉厨还活着吗？ww</div>",
             "count": 8,
             "date": "2030/09/20(金) 20:15:32"
         },
         {
             "id": "tachikawa_bamboo_cafe_staff",
-            "title": "【多摩】立川のbamboo cafeの店員さんについて【喫茶】",
+            "title": "【多摩】立川のbamboo cafeの店員さんについて【喫茶】<div class=\"title-subtitle\">【多摩】有人知道立川Bamboo Cafe那个店员小姐姐的信息吗【茶室】</div>",
             "count": 8,
             "date": "2030/04/12(金) 22:30:00"
         },
         {
             "id": "nishitakeya_staff_childcare",
-            "title": "【立川】西竹屋の店員だけど、最近の世の中終わってね？【子育て】",
+            "title": "【立川】西竹屋の店員だけど、最近の世の中終わってね？【子育て】<div class=\"title-subtitle\">【立川】我是西竹屋的店员，最近这世道是不是完了？【育儿】</div>",
             "count": 10,
             "date": "2030/07/13(土) 09:20:05"
         },
@@ -32,37 +32,37 @@ app.setThreadList({
         },
         {
             "id": "sakayori_lab_job",
-            "title": "【就職】東大の具身知能研ってどうよ？ボスが美少女天才ってマジ？",
+            "title": "【就職】東大の具身知能研ってどうよ？ボスが美少女天才ってマジ？<div class=\"title-subtitle\">【求职】东大的具身智能研怎么样？听说老板是美少女天才真的假的？</div>",
             "count": 8,
             "date": "2040/06/15(金) 14:15:32"
         },
         {
             "id": "yachiyo_stream_voice",
-            "title": "<span class=\"gold-title\">★【良スレ】先日ヤチヨの配信に乗った声、やっぱいろPだよな？同棲確定か？</span>",
+            "title": "<span class=\"gold-title\">★【良スレ】先日ヤチヨの配信に乗った声、やっぱいろPだよな？同棲確定か？</span><div class=\"title-subtitle\">【精品】那天ヤチヨ直播间里暴露的果然是彩P的声音吧？两个人同居确定？</div>",
             "count": 11,
             "date": "2040/02/14(水) 20:15:32"
         },
         {
             "id": "robot_function_question",
-            "title": "【悲報】ボスにロボットの「ある機能」について聞いたら論名削除されかけた件",
+            "title": "【悲報】ボスにロボットの「ある機能」について聞いたら論名削除されかけた件<div class=\"title-subtitle\">【悲报】问了老板机器人为什么要加\"某些功能\"，结果被威胁要从论文里除名了</div>",
             "count": 9,
             "date": "2041/06/10(月) 22:15:32"
         },
         {
             "id": "todai_embodied_scandal",
-            "title": "【悲報】東大の具身知能研の「あの件」、聞いた？",
+            "title": "【悲報】東大の具身知能研の「あの件」、聞いた？<div class=\"title-subtitle\">【悲报】东大具身智能研究所的\"那件事\"，听说了吗？</div>",
             "count": 10,
             "date": "2040/08/31(土) 19:15:32"
         },
         {
             "id": "usj_encounter",
-            "title": "【悲報】週末に某USJ行ったらラボのボス（教授）に遭遇した、人生終わった",
+            "title": "【悲報】週末に某USJ行ったらラボのボス（教授）に遭遇した、人生終わった<div class=\"title-subtitle\">【悲报】周末去某USJ（环球影城）玩遇到了实验室老板（教授），人生完蛋了</div>",
             "count": 9,
             "date": "2041/10/13(日) 19:15:32"
         },
         {
             "id": "kaguya_revival_war",
-            "title": "【激震】かぐや復活？！いきなりいろP・ヤチヨと3人コラボライブ。集金確定か？",
+            "title": "【激震】かぐや復活？！いきなりいろP・ヤチヨと3人コラボライブ。集金確定か？<div class=\"title-subtitle\">kaguya复活，一上来就是和彩P八千代三人联动live，捞钱预定？</div>",
             "count": 34,
             "date": "2040/09/12(木) 21:05:00"
         }
